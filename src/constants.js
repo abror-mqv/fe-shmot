4f3944ff916eb5a14a911efb0d90a84c4565287b
adymar73583418 <adymarr2005@gmail.com>
2023-08-23 00:27:00 +0600

    wefew

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const BACK_URL = 'https://8459-5-8-11-254.ngrok-free.app:8080'
+export const BACK_URL = 'https://8459-5-8-11-254.ngrok-free.app'
 export const FRONT_URL = 'http://5.8.11.254:3000'
