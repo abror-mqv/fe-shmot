9fcd62d4624eb63528e740642408e993711df1b7
adymar73583418 <adymarr2005@gmail.com>
2023-09-06 08:02:18 +0600

    eqfdew

--- src/components/product/Index.jsx
+++ src/components/product/Index.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import Modal from "@mui/material/Modal";
-import Box from "@mui/material/Box";
 import { Button } from '@mui/material'
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+    Mousewheel,
+    Pagination,
+    Thumbs,
+    Controller,
+    EffectCube,
+} from "swiper";
+import "swiper/css/pagination";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import { BACK_URL } from '../../constants';
-
-const StyledProduct = styled`
-    .Product{
-        
-    }
-`
+import TextField from "@mui/material/TextField";
+import { useState } from 'react';
+SwiperCore.use([Navigation, Pagination, Thumbs, Controller, EffectCube]);
 function Product(props) {
     const { open, setOpen, handleClose, data } = { ...props }
-
+    const [name, setName] = useState(null)
+    const [value, setValue] = useState(0)
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+    const [controlledSwiper, setControlledSwiper] = useState(null);
+    const [currentSlide, setCutterntSlide] = useState(1);
     if (data != undefined) {
         console.log(data)
         const arr = [
@@ -46,6 +54,17 @@ function Product(props) {
                 name: data.color_name_4
             },
         ]
+
+        const ColorRounds = arr.map((el) => {
+            return (
+                <SwiperSlide>
+                    <div className="ColorRound" style={{ backgroundColor: `${el.color}` }}>
+
+                    </div>
+                </SwiperSlide>
+            )
+        })
+
         return (
             <Modal
                 open={open}
@@ -54,8 +73,24 @@ function Product(props) {
                 aria-describedby="modal-modal-description"
             >
                 <div className="StyledBoxModal">
-                    <Swiper navigation={true} modules={[Navigation]} slidesPerView={1}
-                        spaceBetween={0} className="Swiper__main">
+                    <Swiper
+                        navigation={true}
+                        modules={[Navigation]}
+                        slidesPerView={1}
+                        spaceBetween={0}
+                        className="Swiper__main"
+                        // pagination={{
+                        //     clickable: true,
+                        // }}
+                        // controller={{ control: controlledSwiper }}
+                        // thumbs={{
+                        //     swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+                        // }}
+                        // onSlideChange={(swiper) => {
+                        //     console.log("slide change, ", swiper.activeIndex);
+                        //     setCutterntSlide(swiper.activeIndex);
+                        // }}
+                    >
                         {arr.map((el) => {
                             // console.log(el.name) 
                             return (
@@ -67,7 +102,41 @@ function Product(props) {
                             );
                         })}
                     </Swiper>
-                    <h1 className='P_title'>{data.product_name}</h1>
+                    <div className='AddToCart'>
+                        <div className="Liner">
+                            {data.siezes}
+                        </div>
+                        <div className="Counter">
+                            <TextField
+                                variant="outlined"
+                                id="outlined-controlled"
+                                label="Кол-во линеек"
+                                value={name}
+                                type='number'
+                                onChange={(event) => {
+                                    setName(event.target.value);
+                                }}
+                            />
+                        </div>
+                        <div className="AddButton" onClick={() => {
+                            alert(name)
+                        }}>
+
+                        </div>
+                    </div>
+                    <div className="Colors">
+                        Цвета в наличии:
+                        {/* <Swiper
+                            id="thumbs"
+                            spaceBetween={1}
+                            slidesPerView={5}
+                            onSwiper={setThumbsSwiper}
+                            className="HeaderLine"
+                        >
+                            {ColorRounds}
+                        </Swiper> */}
+                    </div>
+                    <h1 className='P_title'>{data.product_name} </h1>
                     <Button
                         variant="contained"
                         className="BoxModal__button"
